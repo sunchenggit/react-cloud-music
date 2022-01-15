0fcbdb5df34267bdf20358e2c95280dda2bf148e
suncheng <157600408@qq.com>
2022-01-15 15:57:15 +0800

    完善歌手列表的开发，包括上拉刷新、下拉刷新，加加载loading，防抖加载时间等

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,33 +1,143 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Horizen from '../../baseUI/horizen-item'
 import { categoryTypes, alphaTypes } from '../../api/config'
-import { NavContainer } from './style'
+import { NavContainer, List, ListItem, ListContainer } from './style'
+import Scroll from '../../baseUI/scroll'
+import { connect } from 'react-redux'
+import {
+  getSingerList,
+  getHotSingerList,
+  changeEeterLoading,
+  changePullUpLoading,
+  refreshmoreSingerList,
+  refreshmoreHotSingerList,
+  changePullDownLoading,
+  changePageCount
+} from './store/actionCreators'
+import Loading from '../../baseUI/loading/index'
+import LazyLoad, {forceCheck} from 'react-lazyload'
 
-function Singers() {
+function Singers(props) {
   let [category, setCategory] = useState('')
   let [alpha, setAlpha] = useState('')
+  const { singerList, enterLoading, pullDownLoading, pullUpLoading, pageCount } = props
+  const { updateDispatch, getHotSingerDispatch, pullupRefreshDispatch, pullDownRefreshDispatch } = props
+
+  useEffect(() => {
+    if(!singerList.size) {
+      getHotSingerDispatch()
+    }
+    //eslint-disable-next-line
+  }, [])
+
   let handleUpdateAlpha = (val) => {
     setAlpha(val)
+    updateDispatch(category, val)
   }
   let handleUpdateCategory = (val) => {
     setCategory(val)
+    updateDispatch(val, alpha)
   }
+
+  const handlePullUp = () => {
+    pullupRefreshDispatch(category, alpha , category === '', pageCount)
+  }
+
+  const handlePullDown = () => {
+    pullDownRefreshDispatch(category, alpha)
+  }
+
+  const renderSingerList = () => {
+    const list = singerList ? singerList.toJS() : []
+    return (
+      <List>
+        {
+          list.map((item, index) => {
+            return (
+              <ListItem key={item.id+''+index}>
+                <div className='img_wrapper'>
+                  <LazyLoad placeholder={<img src={require('./singer.png')} width='100%' height="100%" alt="nusic" />}>
+                    <img src={`${item.picUrl}?param=300*300`} width='100%' height="100%" alt="nusic" />
+                  </LazyLoad>
+                </div>
+                <span className='name'>{item.name}</span>
+              </ListItem>
+            )
+          })
+        }
+      </List>
+    )
+  }
+
   return (
-    <NavContainer>
-      <Horizen
-      list={categoryTypes}
-      title={"分类 (默认热门)"}
-      handleClick={handleUpdateCategory}
-      oldVal={category}
-      ></Horizen>
-      <Horizen
-      list={alphaTypes}
-      title={"首字母:"}
-      handleClick={val => handleUpdateAlpha(val)}
-      oldVal={alpha}
-      ></Horizen>
-    </NavContainer>
+    <div>
+      <NavContainer>
+        <Horizen
+        list={categoryTypes}
+        title={"分类 (默认热门)"}
+        handleClick={handleUpdateCategory}
+        oldVal={category}
+        ></Horizen>
+        <Horizen
+        list={alphaTypes}
+        title={"首字母:"}
+        handleClick={val => handleUpdateAlpha(val)}
+        oldVal={alpha}
+        ></Horizen>
+      </NavContainer>
+      <ListContainer>
+        <Scroll
+          pullUp={handlePullUp}
+          pullDown={handlePullDown}
+          pullDownLoading={pullDownLoading}
+          pullUpLoading={pullUpLoading}
+          onScrol={forceCheck}
+        >
+          { renderSingerList() }
+        </Scroll>
+        <Loading show={enterLoading}></Loading> 
+      </ListContainer>
+    </div>
   )
 }
 
-export default React.memo(Singers)
+const mapStateToProps = state => ({
+  singerList: state.getIn(['singers', 'singerList']),
+  enterLoading: state.getIn(['singers', 'enterLoading']),
+  pullUpLoading: state.getIn(['singers', 'pullUpLoading']),
+  pullDownLoading: state.getIn(['singers', 'pullDownLoading']),
+  pageCount: state.getIn(['singers', 'pageCount']),
+})
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getHotSingerDispatch() {
+      dispatch(getHotSingerList())
+    },
+    updateDispatch(category, alpha) {
+      dispatch(changePageCount(0))
+      dispatch(changeEeterLoading(true))
+      dispatch(getSingerList(category, alpha))
+    },
+    pullupRefreshDispatch(category, alpha, hot, count) {
+      dispatch(changePullUpLoading(true))
+      dispatch(changePageCount(count+1))
+      if (hot) {
+        dispatch(refreshmoreHotSingerList())
+      } else {
+        dispatch(refreshmoreSingerList(category, alpha))
+      }
+    },
+    pullDownRefreshDispatch(category, alpha) {
+      dispatch(changePullDownLoading(true))
+      dispatch(changePageCount(0))
+      if (category === '' && alpha === '') {
+        dispatch(getHotSingerList())
+      } else {
+        dispatch(getSingerList(category, alpha))
+      }
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Singers))
--- src/baseUI/scroll/index.js
+++ src/baseUI/scroll/index.js
@@ -1,7 +1,10 @@
-import React, { forwardRef, useState, useEffect, useRef, useImperativeHandle } from 'react'
+import React, { forwardRef, useState, useEffect, useRef, useImperativeHandle, useMemo } from 'react'
 import PropTypes from 'prop-types'
 import BScroll from 'better-scroll'
 import styled from 'styled-components'
+import Loading from '../loading/index'
+import LoadingV2 from '../loading-v2/index'
+import { debounce } from '../../api/utils'
 
 const ScrollContainer = styled.div`
   width: 100%;
@@ -9,11 +12,41 @@ const ScrollContainer = styled.div`
   overflow: hidden;
 `
 
+const PullUpLoading = styled.div`
+  position: absolute;
+  left:0; right:0;
+  bottom: 5px;
+  width: 60px;
+  height: 60px;
+  margin: auto;
+  z-index: 100;
+`;
+
+export const PullDownLoading = styled.div`
+  position: absolute;
+  left:0; right:0;
+  top: 0px;
+  height: 30px;
+  margin: auto;
+  z-index: 100;
+`;
+
 const Scroll = forwardRef((props, ref) => {
   const [bScroll, setBScroll] = useState()
   const scrollContainerRef = useRef()
   const { direction, click, refresh, bounceTop, bounceBottom } = props
   const { pullUp, pullDown, onScroll } = props
+  const { pullDownLoading, pullUpLoading } = props
+
+  let pullUpDebounce = useMemo(() => {
+    return debounce(pullUp, 300)
+  },[pullUp])
+  // 千万注意，这里不能省略依赖，
+  // 不然拿到的始终是第一次 pullUp 函数的引用，相应的闭包作用域变量都是第一次的，产生闭包陷阱。下同。
+
+  let pullDownDebounce = useMemo(() => {
+    return debounce(pullDown, 300)
+  },[pullDown])
 
   useEffect(() => {
     const scroll = new BScroll(scrollContainerRef.current, {
@@ -45,29 +78,31 @@ const Scroll = forwardRef((props, ref) => {
 
   useEffect(() => {
     if(!bScroll || !pullUp) return
-    bScroll.on('scrollEnd', () => {
+    const handlePullUp = () => {
       // 判断是否滑动到了底部
       if (bScroll.y <= bScroll.maxScrollY + 100) {
-        pullUp()
+        pullUpDebounce()
       }
-    })
-    return () => {
-      bScroll.off('scrollEnd')
     }
-  }, [pullUp, bScroll])
+    bScroll.on('scrollEnd', handlePullUp)
+    return () => {
+      bScroll.off('scrollEnd', handlePullUp)
+    }
+  }, [pullUp, pullUpDebounce, bScroll])
 
   useEffect(() => {
     if (!bScroll || !pullDown) return
-    bScroll.on('touchEnd', (pos) => {
+    const handlePullDown = (pos) => {
       // 判断用户下拉动作
       if (pos.y > 50) {
-        pullDown()
+        pullDownDebounce()
       }
-    })
-    return () => {
-      bScroll.off('touchEnd')
     }
-  }, [bScroll, pullDown])
+    bScroll.on('touchEnd', handlePullDown)
+    return () => {
+      bScroll.off('touchEnd', handlePullDown)
+    }
+  }, [bScroll, pullDownDebounce, pullDown])
 
   useEffect(() => {
     if (refresh && bScroll) {
@@ -89,9 +124,14 @@ const Scroll = forwardRef((props, ref) => {
     }
   }))
 
+  const PullUpdisplayStyle = pullUpLoading ? { display: '' } : { display: "none" }
+  const PullDowndisplayStyle = pullDownLoading ? { display: '' } : { display: "none" }
+
   return (
     <ScrollContainer ref={scrollContainerRef}>
       {props.children}
+      <PullUpLoading style={ PullUpdisplayStyle }><Loading></Loading></PullUpLoading>
+      <PullDownLoading style={ PullDowndisplayStyle }><LoadingV2></LoadingV2></PullDownLoading>
     </ScrollContainer>
   )
 })
